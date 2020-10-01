2d39a2d70b7f2bf617a2fa53c5b74ab0f01ba8aa
ThaGrza <andrewgriswold7@gmail.com>
2020-09-30 19:46:04 -0500

    navbar

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import './header.css';
+import SearchIcon from '@material-ui/icons/Search';
 
 export default function Header() {
   return (
     <div className='header'>
-      <img className ='header_logo' src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' />
-      <div classname='header_search'>
-        <input className='header_searchInput' type='text'></input>
-        {/* Logo */}
+      <img className ='header_logo' alt='🧙‍♂️' src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' />
+      <div className='header_search'>
+        <input className='header_searchInput' type='text'/>
+        <SearchIcon className='header_searchIcon'/>
       </div>
       <div className='header_nav'>
         <div className='header_option'>
-          <span className='header__optionLineOne'>Hello guest</span>
+          <span className='header_optionLineOne'>Hello guest</span>
         </div>
         <div className='header_option'>
-          <span className='header__optionLineOne'>Sign In</span>
+          <span className='header_optionLineOne'>Sign In</span>
         </div>
 
         <div className='header_option'>
