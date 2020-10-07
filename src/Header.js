c48339be6ce5f7aa56bf1b8f9a7217dfff813db4
ThaGrza <andrewgriswold7@gmail.com>
2020-10-06 19:16:22 -0500

    small changes to setup

--- src/Header.js
+++ src/Header.js
@@ -1,36 +1,25 @@
 import React from 'react'
 import './header.css';
 import SearchIcon from '@material-ui/icons/Search';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <div className='header'>
       <img className ='header_logo' alt='🧙‍♂️' src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' />
+
       <div className='header_search'>
         <input className='header_searchInput' type='text'/>
         <SearchIcon className='header_searchIcon'/>
       </div>
-      <div className='header_nav'>
-        <div className='header_option'>
-          <span className='header_optionLineOne'>Hello guest</span>
-        </div>
-        <div className='header_option'>
-          <span className='header_optionLineOne'>Sign In</span>
-        </div>
 
-        <div className='header_option'>
-          <span className='header_optionLineOne'>Returns</span>
-        </div>
-        <div className='header_option'>
-          <span className='header_optionLineOne'>& Orders</span>
-        </div>
-
-        <div className='header_option'>
-          <span className='header_optionLineOne'>Your</span>
-        </div>
-        <div className='header_option'>
-          <span className='header_optionLineOne'>Prime</span>
-        </div>
+      <div className='headerNav'>
+        <Link to='/login' className='header_link'>
+          <div className='header_option'>
+            <span> Hello Andrew</span>
+            <span> Sign In</span>
+          </div>
+        </Link>
       </div>
     </div>
   )
