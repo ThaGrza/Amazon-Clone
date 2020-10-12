3e6cf04aeba8c7cb4fd78f747e6323194b51e58f
ThaGrza <andrewgriswold7@gmail.com>
2020-10-12 15:22:08 -0500

    navbar styling done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+            <Route path="/Checkout">
+              <h1>Checkout</h1>
+            </Route>
+            <Route path="/Login">
+              <h1>Login</h1>
+            </Route>
+            <Route path="/">
+              <Header />
+              <h1>Home Page!</h1>
+            </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,51 @@
 import React from 'react'
 import './header.css';
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'; 
 import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <div className='header'>
-      <img className ='header_logo' alt='🧙‍♂️' src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' />
+      <Link to="/">
+      <img 
+      className ='header_logo' 
+      alt='🧙‍♂️' 
+      src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' />
+      </Link>
+
 
       <div className='header_search'>
         <input className='header_searchInput' type='text'/>
         <SearchIcon className='header_searchIcon'/>
       </div>
 
-      <div className='headerNav'>
+      <div className='header_nav'>
         <Link to='/login' className='header_link'>
           <div className='header_option'>
-            <span> Hello Andrew</span>
-            <span> Sign In</span>
+            <span className="header_optionLineOne"> Hello Andrew</span>
+            <span className="header_optionLineTwo"> Sign In</span>
+          </div>
+        </Link>
+
+        <Link to='/' className='header_link'>
+          <div className='header_option'>
+            <span className="header_optionLineOne">Returns</span>
+            <span className="header_optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+
+        <Link to='/' className='header_link'>
+          <div className='header_option'>
+            <span className="header_optionLineOne">Your</span>
+            <span className="header_optionLineTwo">Prime</span>
+          </div>
+        </Link>
+
+        <Link to='/Checkout' className="header_link">
+          <div className='header_optionBasket'>
+            <ShoppingBasketIcon />
+            <span className='header_optionLineTwo header_basketCount'>0</span>
           </div>
         </Link>
       </div>
